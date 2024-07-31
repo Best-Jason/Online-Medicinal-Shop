7fb6979a6f858390e30b31f527a559d6179c7675
Best-Jason <23022483@myrp.edu.sg>
2024-07-31 12:22:57 +0800

    changed port

--- app.js
+++ app.js
@@ -243,5 +243,5 @@ app.post('/editBasketItem/:id', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
